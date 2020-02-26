419d0a62675193c021faa55cd92d26796b364137
GrMrWb <56293658+GrMrWb@users.noreply.github.com>
2020-02-26 15:43:49 +0000

    Progression on Live Video

--- UserInterface/static/home/js/data.js
+++ UserInterface/static/home/js/data.js
@@ -4,6 +4,10 @@ var data
 function updateData(){
     $.getJSON("/static/home/js/data.json",function(data){
         if (data["v"]!=pastversion){
+            //Environmental Data
+            document.getElementById("warnenv").innerText= data["Warning"]["seagull"]== "1" ? "SeaGulls in Vicinity" : "NoWarning";
+            document.getElementById("titled_warnenv").style.backgroundColor = data["Warning"]["seagull"]== "1" ? "orange" : "green";
+
             //TriTrack Data
             document.getElementById("PiStat").innerText = data["TriTrackDataMic"]["PiStat"] == "1" ? "Not Available" : "Available for Processing";
             document.getElementById("Pi4procTri").innerText = data["TriTrackDataMic"]["Pi4procTri"]+" %";
@@ -11,8 +15,8 @@ function updateData(){
             document.getElementById("BrdProc").innerText = data["TriTrackDataMic"]["BrdProc"]+" %";
 
             // PoweBoard
-            document.getElementById("Consumption").innerText = data["TriTrackPowerBoard"]["Consumption"];
-            document.getElementById("Consumptiona").innerText = data["TriTrackPowerBoard"]["Consumption"];
+            document.getElementById("Consumption").innerText = data["TriTrackPowerBoard"]["Consumption"] + ' W';
+            document.getElementById("Consumptiona").innerText = data["TriTrackPowerBoard"]["Consumption"] + ' W';
             document.getElementById("CapA").innerHTML = '<image style="height:25px;" src=/static/home/src/' + data["TriTrackPowerBoard"]["CapA"]+'.svg />';
             document.getElementById("CapB").innerHTML = '<image style="height:25px;" src=/static/home/src/' + data["TriTrackPowerBoard"]["CapB"]+'.svg />';
             
